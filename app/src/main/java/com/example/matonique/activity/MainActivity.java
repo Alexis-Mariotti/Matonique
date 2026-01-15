74ebc9deb9814f4260dbe9626a20eefff243a7f6
Loëvann <newlolo510@gmail.com>
2026-01-15 17:21:16 +0100

    navbar

--- app/src/main/java/com/example/matonique/activity/MainActivity.java
+++ app/src/main/java/com/example/matonique/activity/MainActivity.java
@@ -11,6 +11,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.matonique.R;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import fragments.HomeFragment;
+import fragments.PlayingFragment;
+import fragments.PlaylistFragment;
+import fragments.SettingsFragment;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -21,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        // Afficher le fragment d'accueil par défaut au démarrage
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -30,4 +50,28 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    private final BottomNavigationView.OnItemSelectedListener navListener = item -> {
+        // Par défaut, fragment sélectionné est null
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_home) {
+            selectedFragment = new HomeFragment();
+        } else if (itemId == R.id.nav_playing) {
+            selectedFragment = new PlayingFragment();
+        } else if (itemId == R.id.nav_playlist) {
+            selectedFragment = new PlaylistFragment();
+        } else if (itemId == R.id.nav_settings) {
+            selectedFragment = new SettingsFragment();
+        }
+
+        // Remplacer le fragment
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+        }
+        return true;
+    };
 }
