b2aedf3e6158ca4cba52cffe58a2883536891bfd
Alexis-Mariotti <alexis.mariotti@laposte.net>
2026-06-03 15:08:44 +0200

    ajout dependences mockito, roboelectric and expresso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,6 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     testImplementation("androidx.test:core:1.5.0")
     testImplementation("androidx.test.ext:junit:1.1.5")
-    testImplementation("androidx.test.espresso:espresso- core:3.5.1")
+    testImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("org.robolectric:robolectric:4.11.1")
 }
